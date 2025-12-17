570682f5f8517a770afb9c7f9ad7818c23a2f61a
Tunuguntla Mohana Sai <mohana.sai1204@gmail.com>
2025-12-17 23:04:57 +0530

    [Tunuguntla Mohana Sai] implemented the remaining basic core java problems

---
+++ core-java-practice/gcr-codebase/programming-elements/VolumeOfCylinder.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class VolumeOfCylinder {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter radius: ");
+		double radius = sc.nextDouble();
+		
+		System.out.print("Enter height: ");
+		double height = sc.nextDouble();
+		
+		//Math.PI gives the pie value(22/7)
+		double volume = Math.PI * radius * radius * height;
+		
+		System.out.println("Volume of the cylinder = " + volume);
+		sc.close();
+	}
+}
