c5e9f49821f38572e77bafc1b1ba3192c95f9303
Tunuguntla Mohana Sai <mohana.sai1204@gmail.com>
2025-12-23 16:18:40 +0530

    [Tunuguntla Mohana Sai] Add. solved string handling practice questions

---
+++ core-java-practice/gcr-codebase/string-handling/LongestWordInSentence.java
@@ -0,0 +1,21 @@
+package com.corejavapractice.gcrcodebase.stringpractice;
+
+import java.util.Scanner;
+
+public class LongestWordInSentence {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a sentence: ");
+		String sentence = sc.nextLine();
+
+		String[] words = sentence.split("\s+");
+		String longest = "";
+		for (String word : words) {
+			if (word.length() > longest.length()) {
+				longest = word;
+			}
+		}
+		System.out.println("Longest word: " + longest);
+		sc.close();
+	}
+}
---
+++ core-java-practice/gcr-codebase/string-handling/SubstringOccurances.java
@@ -0,0 +1,22 @@
+package com.corejavapractice.gcrcodebase.stringpractice;
+
+import java.util.Scanner;
+
+public class SubstringOccurances {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter main string: ");
+		String mainStr = sc.nextLine();
+		System.out.print("Enter substring to find: ");
+		String subStr = sc.nextLine();
+
+		int count = 0;
+		for (int i = 0; i <= mainStr.length() - subStr.length(); i++) {
+			if (mainStr.substring(i, i + subStr.length()).equals(subStr)) {
+				count++;
+			}
+		}
+		System.out.println("Number of occurrences: " + count);
+		sc.close();
+	}
+}
